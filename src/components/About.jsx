fa11d2fec9f0a812d6a5f22439f14af12a1ce1b7
KHELIL Akram <khelili.akram@courrier.uqam.ca>
2024-12-08 05:03:40 -0500

    add work section model , problem with center text 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,12 +4,12 @@ const About = () => {
   return (
     <div className='text-white max-w-[1200px] mx-auto' id='about'>
       <div className='md:grid md:grid-cols-2 sm:py-16'>
-        <div className='mt-4 md:mt-0 text-left  flex '>
-            <div className='my-auto mx-6 '>
-                <h2 className='text-4xl font-bold mb-4 primary-color'>À propos de moi</h2>
-                <p className='text-base lg:text-lg '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus dolorum ipsam, qui unde tempore aspernatur veniam ducimus consequatur necessitatibus odio ea aut doloremque, similique ab odit ex nemo explicabo!</p>
+        <div className='mt-4 md:mt-0 text-left '>
+        <div className='my-auto mx-6 text-left'>
+              <h2 className='text-4xl font-bold mb-4 primary-color'>À propos de moi</h2>
+              <p className='text-base lg:text-lg'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus minus dolorum ipsam, qui unde tempore aspernatur veniam ducimus consequatur necessitatibus odio ea aut doloremque, similique ab odit ex nemo explicabo!</p>
+        </div>
 
-            </div>
         </div>
         <img className='mx-auto rounded-3xl py-8 md:py-0' width={300} height={300} src={akram} alt="" />
       </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import projetMaitrise from '../assets/ReaserachPaper.webp' 
+import projetLicence from '../assets/projetLicence.webp'
 const Work = () => {
   return (
     <div className='max-w-[1200px] mx-auto p-5' id='work'>
-      <div className='pb-8'>
+      <div className='pb-8 text-left'>
         <p className='text-4xl mb-3 font-bold primary-color'>Mes traveaux</p>
         <p className='text-gray-400'>Explorez mes récents projets</p>
       </div>
-      <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 '>
-        <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden  shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div h-[200px] bg-cover relative'>
-          <img src={projetMaitrise} alt=""  layout="fill" objectFit="cover"/>
-           <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
-              <a href="/">
-                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Direct
-                </button>
-              </a>
-           </div>
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+      <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div h-[300px] md:h-[350px] lg:h-[400px] bg-cover relative'>
+        <img src={projetMaitrise} alt="" layout="fill" objectFit="cover"/>
+        <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
+          <a href="/">
+            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              Direct
+            </button>
+          </a>
         </div>
-
       </div>
+      <div className='transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div h-[300px] md:h-[350px] lg:h-[400px] bg-cover relative'>
+        <img src={projetLicence} alt="" layout="fill" objectFit="cover"/>
+        <div className='opacity-0 group-hover:opacity-90 bg-[gray]/70 absolute inset-0 flex flex-col justify-center items-center'>
+          <a href="/">
+            <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+              Direct
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
+
     </div>
   )
 }
